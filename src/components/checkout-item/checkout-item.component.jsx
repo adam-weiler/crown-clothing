e9742a60c810fc834d40804471791013fa5db2c1
Adam Weiler <adam.weiler@yahoo.ca>
2022-04-15 18:21:12 -0400

    Using styled-components for cart-dropdown, cart-icon, cart-item, category-review, checkout-item, and directory-item.

--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -2,7 +2,7 @@ import { useContext } from 'react';
 
 import { CartContext } from '../../contexts/cart.context';
 
-import './checkout-item.styles.scss';
+import { CheckoutItemContainer, ImageContainer, Quantity, RemoveButton } from './checkout-item.styles.jsx';
 
 const CheckoutItem = ({ cartItem }) => {
     const { name, imageUrl, price, quantity } = cartItem;
@@ -13,12 +13,12 @@ const CheckoutItem = ({ cartItem }) => {
     const clearItemHandler = () => clearItemFromCart(cartItem);
 
     return (
-        <div className='checkout-item-container'>
-            <div className='image-container'>
+        <CheckoutItemContainer>
+            <ImageContainer>
                 <img src={imageUrl} alt={`${name}`} />
-            </div>
+            </ImageContainer>
             <span className='name'>{name}</span>
-            <span className='quantity'>
+            <Quantity>
                 <div className='arrow' onClick={removeItemHandler}>
                     &#10094;
                 </div>
@@ -26,10 +26,10 @@ const CheckoutItem = ({ cartItem }) => {
                 <div className='arrow' onClick={addItemHandler}>
                     &#10095;
                 </div>
-            </span>
+            </Quantity>
             <span className='price'>{price}</span>
-            <div className='remove-button' onClick={clearItemHandler}>&#10005;</div>
-        </div>
+            <RemoveButton onClick={clearItemHandler}>&#10005;</RemoveButton>
+        </CheckoutItemContainer>
     )
 }
 
