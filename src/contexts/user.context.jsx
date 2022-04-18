640d41e98952a5e29f79743400a7b16b5ee2a8d6
Adam Weiler <adam.weiler@yahoo.ca>
2022-04-18 16:37:54 -0400

    Converting user context from state to Reducers.

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,6 +1,5 @@
 // import { getByDisplayValue } from '@testing-library/react';
-import { createContext, useState, useEffect } from 'react';
-
+import { createContext, useState, useEffect, useReducer } from 'react';
 import { onAuthStateChangedListener, createUserDocumentFromAuth } from '../utils/firebase/firebase.utils'
 
 // The actual value you want to access.
@@ -9,8 +8,48 @@ export const UserContext = createContext({  // Default values.
     setCurrentUser: () => null,
 });
 
+
+
+
+
+export const USER_ACTION_TYPES = {
+    'SET_CURRENT_USER': 'SET_CURRENT_USER'
+}
+
+const userReducer = (state, action) => {
+    console.log('Dispatched was called')
+    console.log(action)
+    const { type, payload } = action;
+
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state, // The previous values on the state object.
+                currentUser: payload // And overwrite with the new value.
+            }
+        default:
+            throw new Error(`Unhandled type ${type} in userReducer.`);
+    }
+}
+
+const INITIAL_STATE = {
+    currentUser: null
+}
+
+
+
 export const UserProvider = ({ children }) => { // Any children of UserProvider can access this state.
-    const [currentUser, setCurrentUser] = useState(null);
+    // const [currentUser, setCurrentUser] = useState(null);
+
+    const [ state, dispatch ] = useReducer(userReducer, INITIAL_STATE);
+    const { currentUser } = state;
+    console.log(currentUser)
+
+
+    const setCurrentUser = (user) => {
+        dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user })
+    }
+
     const value= { currentUser, setCurrentUser };
 
     useEffect(() => { // It checks the authentication state automatically when this function mounts.
@@ -30,3 +69,19 @@ export const UserProvider = ({ children }) => { // Any children of UserProvider 
 //<UserProvider>
 //  <App/>  //<App/> is the children in this case.
 //</UserProvider>
+
+/*
+Reducers: Functions that return back an object.
+const userReducer = () => {
+    return {
+        currentUser: null, or object from Firebase.
+    }
+}
+
+const userReducer = (state, action) => {
+    return {
+        currentUser: null, or object from Firebase.
+    }
+}
+
+*/
