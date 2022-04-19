caccf5d2fa27a302838c04567b302893f2cd8d98
Adam Weiler <adam.weiler@yahoo.ca>
2022-04-19 18:29:14 -0400

    Installed Redux. Creating user-reducer and root-reducer.

---
+++ src/store/store.js
@@ -0,0 +1,4 @@
+// Redux:
+// This is where our state lives.
+// Where we receive actions.
+// And dispatch to the Reducers to update the state.
---
+++ src/store/user/user.reducer.js
@@ -0,0 +1,23 @@
+export const USER_ACTION_TYPES = {
+    'SET_CURRENT_USER': 'SET_CURRENT_USER'
+};
+
+const INITIAL_STATE = {
+    currentUser: null
+};
+
+export const userReducer = (state = INITIAL_STATE, action) => {
+    // console.log('Dispatched was called')
+    // console.log(action)
+    const { type, payload } = action;
+
+    switch(type) {
+        case USER_ACTION_TYPES.SET_CURRENT_USER:
+            return {
+                ...state, // The previous values on the state object.
+                currentUser: payload // And overwrite with the new value.
+            }
+        default: // Every reducer is called for each action. However, none of the cases matched, so this part of our reducer did not change.
+            return state; // Therefore we need to return the state.
+    }
+};
